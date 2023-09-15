21858b7bf436791dcc347e90caa0bd890652960a
giuliaricciuti <giuliaricciuti@localhost>
2023-09-15 12:32:31 +0200

    prova

--- src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -1,25 +1,32 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.spellchecker;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.StringTokenizer;
+
+import it.polito.tdp.spellchecker.model.Dictionary;
+import it.polito.tdp.spellchecker.model.RichWord;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class FXMLController {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
-    private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
-    private URL location;
+	    @FXML // ResourceBundle that was given to the FXMLLoader
+	    private ResourceBundle resources;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
+	    @FXML // URL location of the FXML file that was given to the FXMLLoader
+	    private URL location;
 
-    }
+	    @FXML // This method is called by the FXMLLoader when initialization is complete
+	    void initialize() {
+
+	    }
+
 }
 
 
---
+++ src/main/java/it/polito/tdp/spellchecker/model/Dictionary.java
@@ -0,0 +1,11 @@
+package it.polito.tdp.spellchecker.model;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+public class Dictionary {
+
+
+}
